cc9d94d5e2408af01019b5a56e5ac01fa54c2b89
Roney George <georgeroney@hotmail.com>
2019-10-31 22:03:12 -0400

    updated reame with postgres instructions added adonetcluster membership

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# orleansdemo
+# orleansdemo
 
-## co-hosting
+## co-hosting
 
-## directclient
-  
-## mongdb storage
-  
-## dashboard
-  
-## transactions
+## directclient
+  
+## postgres-db
+1. create a database with name 'orleans'
+2. create a user with name 'orleans' and password 'orleans'
+3. run this script - https://github.com/dotnet/orleans/blob/master/src/AdoNet/Shared/PostgreSQL-Main.sql
+4. run this script - https://raw.githubusercontent.com/dotnet/orleans/master/src/AdoNet/Orleans.Clustering.AdoNet/PostgreSQL-Clustering.sql
+5. run this script - https://github.com/dotnet/orleans/blob/master/src/AdoNet/Orleans.Persistence.AdoNet/PostgreSQL-Persistence.sql
+6. run this script - https://github.com/dotnet/orleans/blob/master/src/AdoNet/Orleans.Reminders.AdoNet/PostgreSQL-Reminders.sql
+  
+## dashboard
+  
+## transactions
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,47 +1,81 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Orleans.Hosting;
-using Orleans;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans.Hosting;
+using Orleans;
+using Orleans.Configuration;
+using System;
+using System.Net;
 
-namespace WebApi
+namespace WebApi
 {
-	public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new HostBuilder()
-                .ConfigureWebHostDefaults(builder =>
-                {
-                    builder.UseStartup<Startup>();
-                })
-                //https://stackoverflow.com/questions/54841844/orleans-direct-client-in-asp-net-core-project/54842916#54842916
-                .UseOrleans(builder =>
-                {
-                    // EnableDirectClient is no longer needed as it is enabled by default
-                    builder.UseLocalhostClustering()
-                    .ConfigureApplicationParts(parts =>
-                        parts.AddApplicationPart(typeof(grains.IUserGrain).Assembly).WithReferences())
-                    .AddAdoNetGrainStorageAsDefault(options => 
-                    {
-                        options.Invariant = "Npgsql";
-                        options.ConnectionString = "host=localhost;database=orleans;username=agentpowers";
-                        options.UseJsonFormat = true;
-                    })
-                    .ConfigureLogging(x =>
-                    {
-                        x.AddConsole();
-                        x.SetMinimumLevel(LogLevel.Warning);
-                    })
-                    .UseDashboard(x =>
-                    {
-                        x.HostSelf = false;
-                    })
-                    .UseTransactions();
-                })
-                .Build();
-            host.Run();
-        }
-    }
+    public class Program
+    {
+        private const string Invariant = "Npgsql";
+        private const string ConnectionString = "host=localhost;database=orleans;username=orleans;password=orleans";
+
+        public static void Main(string[] args)
+        {
+            int apiPort, siloPort, gatewayPort;
+            try
+            {
+                apiPort = int.Parse(args[0]);
+                siloPort = int.Parse(args[1]);
+                gatewayPort = int.Parse(args[2]);
+            }
+            catch (Exception)
+            {
+                apiPort = 50000;
+                siloPort = 11111;
+                gatewayPort = 30000;
+            }
+            var host = new HostBuilder()
+                .ConfigureWebHostDefaults(builder =>
+                {
+                    builder.UseStartup<Startup>();
+                    builder.UseUrls($"http://*:{apiPort}");
+                })
+                //https://stackoverflow.com/questions/54841844/orleans-direct-client-in-asp-net-core-project/54842916#54842916
+                .UseOrleans(builder =>
+                {
+                    // EnableDirectClient is no longer needed as it is enabled by default
+                    builder.Configure<ClusterOptions>(options =>
+                    {
+                        options.ClusterId = "my-first-cluster";
+                        options.ServiceId = "netcore3";
+                    })
+                    .ConfigureEndpoints(siloPort: siloPort, gatewayPort: gatewayPort)
+                    .UseAdoNetClustering(options => 
+                    {
+                        options.Invariant = Invariant;
+                        options.ConnectionString = ConnectionString;
+                    })
+                    .AddAdoNetGrainStorageAsDefault(options => 
+                    {
+                        options.Invariant = Invariant;
+                        options.ConnectionString = ConnectionString;
+                        options.UseJsonFormat = true;
+                    })
+                    .ConfigureApplicationParts(parts =>
+                        parts.AddApplicationPart(typeof(grains.IUserGrain).Assembly).WithReferences())
+                    .ConfigureLogging(x =>
+                    {
+                        x.AddConsole();
+                        x.SetMinimumLevel(LogLevel.Warning);
+                    })
+                    .UseDashboard(x =>
+                    {
+                        x.HostSelf = false;
+                    })
+                    .UseTransactions();
+                })
+                .Build();
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
 }
